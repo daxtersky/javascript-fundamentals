30ce9a1a33aef1d8ead71add4064e368815002c2
Mikolaj Kla <daxtersky@gmail.com>
2019-10-30 20:49:09 +0100

    typeof

---
+++ cl/04 method typeof.js
@@ -0,0 +1,15 @@
+/* typeOf */
+
+const vnull = null;
+const vundefined = undefined;
+const vnan = NaN;
+const vstring = 'string variable';
+const vnum = 1983;
+const vboolean = true;
+
+console.log('-null- is a(n)', typeof vnull);
+console.log('-undefined- is a(n)', typeof vundefined);
+console.log('-NaN- is a(n)', typeof vnan);
+console.log('-string- is a(n)', typeof vstring);
+console.log('-num- is a(n)', typeof vnum);
+console.log('-boolean- is a(n)', typeof vboolean);
