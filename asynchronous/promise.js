bcf45b76c4ca6ba12523e8e317e9daf4b9e161a3
Miko <daxtersky@gmail.com>
2021-02-13 19:50:49 +0100

    small practice on Promises

--- asynchronous/promise.js
+++ asynchronous/promise.js
@@ -7,3 +7,27 @@ const buildSuccessfullRelationship = new Promise((resolve, reject) => {
   .then(() => console.log('I love you too! :)'))
   .catch(() => console.log('No, look for another one, not me... :('))
   .finally(() => console.log("...if you don't ask you will never now."));
+
+// ! https://dev.to/qmaximillian/snow-days-and-javascript-promises-48id
+function willItSnow() {
+  // return false;
+  return Math.random() < 0.5;
+}
+
+console.log('Will it snow tomorrow?....');
+const snowPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    willItSnow()
+      ? resolve('Snow is coming!....')
+      : reject('No snow, spring is closer....');
+  }, 1000);
+});
+
+snowPromise
+  .then(
+    (value) => console.log('.then value:', value), // fulfilled
+    // (error) => console.log('.then error:', error) // rejected
+  )
+  .catch((error) => console.log('.catch error', error));
+
+
