5620573fa3853bdb3764256341c8feb715f8a101
Miko <mikizuk@gmail.com>
2021-02-20 16:43:19 +0100

    expression vs statement in js

---
+++ expression-statement.js
@@ -0,0 +1,32 @@
+// * STATEMENT VS EXPRESSION
+
+// ! https://medium.com/launch-school/javascript-expressions-and-statements-4d32ac9c0e74
+
+// * EXPRESSION - unit of code that can be evaluated to a value
+
+// arithmetic expression
+10;
+10+3;
+// string expression
+'hello world';
+'hello' + 'world';
+// logical expression
+10>9;
+true;
+a === 15 || a === 16;
+// assignment expression
+a = 14;
+
+// * STATEMENT - instruction to perform specific action
+
+// declaration statement
+const b;
+const c = 19;
+// function declaration statement
+function foo() {}
+// conditional statement
+if (expression) {
+  // statement 1;
+} else {
+  // statement 2;
+}
