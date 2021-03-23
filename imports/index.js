74993084e22bd88f7863b1cdece5b23d80219445
Miko <mikizuk@gmail.com>
2021-03-23 17:34:34 +0100

    module exports/imports

--- imports/index.js
+++ imports/index.js
@@ -1,5 +1,12 @@
 // * RUN with VS Code's "Live Server" extention and see results in browser's debugger
-import { moduleFunction, foo } from './utils.js';
 
-console.log('....', moduleFunction());
-console.log('....', foo);
+// ! https://ui.dev/esmodules/
+
+import { moduleFunction, foo } from './utils.js'; // 1 way
+// import * as utils from './utils.js'; // 2 way
+import defaultExportedFunction from './utils2.js'; //
+
+console.log('sxport...', moduleFunction());
+console.log('sxport...', foo);
+
+console.log('default export...', defaultExportedFunction());
---
+++ imports/utils2.js
@@ -0,0 +1,3 @@
+export default function defaultExportedFunction() {
+  return 'imported function!';
+}
