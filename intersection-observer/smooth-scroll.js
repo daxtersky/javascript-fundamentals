0f6a121e4cc118a12648db292ab06dd08a42d12a
Miko <mikizuk@gmail.com>
2021-03-24 13:32:50 +0100

    found great smooth scroll resources

---
+++ intersection-observer/smooth-scroll.js
@@ -0,0 +1,5 @@
+// ! https://www.bram.us/2020/01/10/smooth-scrolling-sticky-scrollspy-navigation/
+// ! https://www.youtube.com/watch?v=_5Bu3JY-ZHc
+// ! https://alligator.io/js/intersection-observer/
+// ! https://css-tricks.com/a-few-functional-uses-for-intersection-observer-to-know-when-an-element-is-in-view/
+// ! https://dev.to/lapstjup/bare-minimum-infinite-scroll-ui-2jd0
